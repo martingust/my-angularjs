e4502884d9fafd7c53c6d37c561138ca274a4761
Martin Gustafsson <gusten86@gmail.com>
2014-07-21 07:55:54 +0200

    First commit

---
+++ src/scope.js
@@ -0,0 +1,46 @@
+/* jshint globalstrict: true */
+'use strict';
+
+function Scope() {
+    this.$$watchers = [];
+}
+
+function initWatchVal() {
+}
+
+Scope.prototype.$watch = function (watchFn, listenerFn) {
+    var watcher = {
+        watchFn: watchFn,
+        listenerFn: listenerFn || function () {
+        },
+        last: initWatchVal
+    };
+    this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function () {
+    var ttl = 10;
+    var dirty;
+    do {
+        dirty = this.$$digestOnce();
+        if(dirty && !(ttl--)){
+            throw "10 digest iteration reached";
+        }
+    } while (dirty);
+};
+
+Scope.prototype.$$digestOnce = function () {
+    var self = this;
+    var newValue, oldValue, dirty;
+    _.forEach(this.$$watchers, function (watcher) {
+        newValue = watcher.watchFn(self);
+        oldValue = watcher.last;
+        if (newValue !== oldValue) {
+            watcher.last = newValue;
+            watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), self);
+            dirty = true;
+        }
+    });
+    return dirty;
+};
+
